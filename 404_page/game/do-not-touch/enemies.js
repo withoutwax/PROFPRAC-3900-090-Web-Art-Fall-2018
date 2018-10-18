d1e004b48fe50f4b27b93896c34e2009f7f90e7f
Will Kim <rlagmlckd@gmail.com>
2018-10-18 03:01:35 -0500

    Updated few codes

---
+++ 404_page/game/do-not-touch/enemies.js
@@ -0,0 +1,11 @@
+class Enemies {
+    constructor(x, y, speed, angle, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.angle = angle;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+}
--- 404_page/game/do-not-touch/main.js
+++ 404_page/game/do-not-touch/main.js
@@ -3,10 +3,18 @@ let ballRadius = 10;
 let enemyRadius = 3;
 let score = 0;
 let lives = 3;
+let x, y; // FOR MOUSE CURSOR
+let enemyX, enemyY; // FOR ENEMY
+let noEnemies = 2;
+let dx = 4; // Enemy Ball Speed x-axis
+let dy = -4; // Enemy Ball Speed y-axis
 
 window.onload = function() {
     canvas = document.getElementById("gameCanvas");
     ctx = canvas.getContext("2d");
+
+    enemyX = canvas.width/2;
+    enemyY = canvas.height/2;
 
     let framesPerSecond = 30;
     setInterval(draw, 1000/framesPerSecond);
@@ -15,7 +23,6 @@ window.onload = function() {
 }
 
 // CONFIGURING THE MOUSE MOVEMENT
-let x, y;
 function mouseMoveHandler(e) {
     let relativeX = e.clientX - canvas.offsetLeft;
     // let relativeY = e.clientY - canvas.offsetRight;
@@ -32,6 +39,34 @@ function drawBall() {
     ctx.fillStyle = "#3d3d3d";
     ctx.fill();
     ctx.closePath();
+}
+
+// DRAWING THE BALL OF THE ENEMY
+function drawEnemyBall(randomX, randomY) {
+    ctx.beginPath();
+    ctx.arc(enemyX, enemyY, enemyRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#3d3d3d";
+    ctx.fill();
+    ctx.closePath();
+
+    // MAKING SURE THE ENEMY BALL STAY WITHIN THE CANVAS
+    if(enemyX + dx > canvas.width-enemyRadius || enemyX + dx < enemyRadius) {
+      dx = -dx;
+    }
+    if(enemyY + dy > canvas.height-enemyRadius || enemyY + dy < enemyRadius) {
+      dy = -dy;
+    } else {
+        // COLLISION DETECTION
+
+        if ((enemyX + enemyRadius/2) > (x - ballRadius/2) && (enemyX - enemyRadius/2) < (x + ballRadius/2) && (enemyY + enemyRadius) > (y - ballRadius/2) && (enemyY - enemyRadius/2) < (y + ballRadius/2)) {
+            // alert("You died!");
+            console.log("Collision Detected!");
+        }
+
+    }
+
+    enemyX += dx;
+    enemyY += dy;
 }
 
 // DISPLAY SCORE
@@ -59,6 +94,13 @@ function timer() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height); // USE TO REFRESH EACH PAGE (so it's not a paint job)
     drawBall();
+
+    for (let i = 0; i < noEnemies; i++) {
+        let randomX = 1;
+        let randomY = Math.floor(Math.random() * 2);
+        drawEnemyBall(randomX, randomY);
+    }
+
     drawScore();
     drawLives();
     timer();
